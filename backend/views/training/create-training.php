c5b1330e932a94123593050d27aecec789a0a1b3
saurabh <itqdegrees@gmail.com>
2018-06-08 15:27:21 +0530

    Attachment dynamic issue

--- backend/views/training/create-training.php
+++ backend/views/training/create-training.php
@@ -15,20 +15,22 @@ use yii\helpers\Html;
 use dosamigos\fileupload\FileUploadUI;
 
 ?>
-<div class="row">
-    <div class="col-md-12">
-        <ul class="cd-breadcrumb">
-            <li class="next">
-                <a href="javascript:void(0)">Training</a></li>
-            <li class="active"><a href="<?= Url::toRoute(['user/add-group-members', 'tid' => Yii::$app->samparq->encryptUserData($tid)])?>">Group Management</a></li>
-            <li class="current" > <a href="javascript:void(0)">Step 3: ADD Trainees/Attachment</a></li>
-            <li class="next" > <a>Assessment/Quiz Setting</a></li>
-            <li class="next" > <a >Add/Update Questions</a></li>
-        </ul>
-        <h1>Step 3: Add Trainees/Add attachment</h1>
+    <div class="row">
+        <div class="col-md-12">
+            <ul class="cd-breadcrumb">
+                <li class="next">
+                    <a href="javascript:void(0)">Training</a></li>
+                <li class="active"><a
+                            href="<?= Url::toRoute(['user/add-group-members', 'tid' => Yii::$app->samparq->encryptUserData($tid)]) ?>">Group
+                        Management</a></li>
+                <li class="current"><a href="javascript:void(0)">Step 3: ADD Trainees/Attachment</a></li>
+                <li class="next"><a>Assessment/Quiz Setting</a></li>
+                <li class="next"><a>Add/Update Questions</a></li>
+            </ul>
+            <h1>Step 3: Add Trainees/Add attachment</h1>
         </div>
 
-        <div class="col-md-12">         
+        <div class="col-md-12">
             <div class="whitebox">
                 <section class="content">
                     <div class="mailboxadiin">
@@ -39,28 +41,28 @@ use dosamigos\fileupload\FileUploadUI;
                                         'id' => 'composeForm',
                                     ]); ?>
                                     <?php
-                                        $traineeList = Yii::$app->samparq->getTraineesList($tid);
-                                        $traineeModel->user_id  = $traineeList;
+                                    $traineeList = Yii::$app->samparq->getTraineesList($tid);
+                                    $traineeModel->user_id = $traineeList;
                                     ?>
                                     <div class="row">
-                                    <div class="col-lg-4">
-                                    <?= $form->field($traineeModel, 'user_id')->widget(Select2::classname(), [
-                                        'data' => $uList,
-                                        'options' => ['placeholder' => 'Add Trainees', 'multiple' => true],
-                                        'pluginOptions' => [
-                                            'id' => 'senderList',
-                                            'tokenSeparators' => [',', '']
-                                        ],
-                                    ])->label('Add Trainees'); ?>
+                                        <div class="col-lg-4">
+                                            <?= $form->field($traineeModel, 'user_id')->widget(Select2::classname(), [
+                                                'data' => $uList,
+                                                'options' => ['placeholder' => 'Add Trainees', 'multiple' => true],
+                                                'pluginOptions' => [
+                                                    'id' => 'senderList',
+                                                    'tokenSeparators' => [',', '']
+                                                ],
+                                            ])->label('Add Trainees'); ?>
                                         </div>
-                                        </div>
+                                    </div>
 
                                     <label class="control-label" for="notification-message">Add Material</label>
                                     <?= FileInput::widget([
                                         'model' => $tMaterial,
                                         'attribute' => 'new_name',
-                                        'options'=>[
-                                            'multiple'=>true,
+                                        'options' => [
+                                            'multiple' => true,
                                         ],
                                         'pluginOptions' => [
                                             'uploadUrl' => Url::to(['/training/upload-files/']),
@@ -71,20 +73,22 @@ use dosamigos\fileupload\FileUploadUI;
                                                             tid: $tid,
                                                         };
                                                          }"),
-                                            'allowedFileExtensions'=>['mp4','pdf','xlsx','xls','doc','docx','ppt','pptx'],
-                                            'initialPreviewAsData'=> true,
+                                            'allowedFileExtensions' => ['mp4', 'pdf', 'xlsx', 'xls', 'doc', 'docx', 'ppt', 'pptx'],
+                                            'initialPreviewAsData' => true,
                                             'showPreview' => true,
                                         ]
                                     ]); ?>
                                     <div class="error-msg" style="color: red"></div>
                                     <div class="clearfix"></div>
 
-                                    <div class="alert alert-success fade in" style="background: #bce8f1; border: #bce8f1; color: #31708f">
-                                        <strong>Note: </strong> Please consider bold keyword as Youtube Url ID, https://www.youtube.com/watch?v=<strong>PmFWzWbziz0</strong>
+                                    <div class="alert alert-success fade in"
+                                         style="background: #bce8f1; border: #bce8f1; color: #31708f">
+                                        <strong>Note: </strong> Please consider bold keyword as Youtube Url ID,
+                                        https://www.youtube.com/watch?v=<strong>PmFWzWbziz0</strong>
                                     </div>
 
                                     <?php $trainingModel->youtube_url = explode(',', $trainingModel->youtube_url) ?>
-<br>
+                                    <br>
                                     <?= $form->field($trainingModel, 'youtube_url')->widget(Select2::classname(), [
                                         'options' => ['placeholder' => 'Example: ruHMqQJ6PNE, bxuHJtQJ8XFG etc..', 'multiple' => true],
                                         'pluginOptions' => [
@@ -93,55 +97,135 @@ use dosamigos\fileupload\FileUploadUI;
                                             'allowClear' => true
                                         ],
                                         'pluginEvents' => [
-                                            "select2:select"=>"function() {tt()}",
-                                            "select2:unselecting"=>"function() {tt()}",
+                                            "select2:select" => "function() {tt()}",
+                                            "select2:unselecting" => "function() {tt()}",
                                         ]
                                     ])->label('Add Youtube Ids'); ?>
                                     <div class="alert alert-danger hide" id="errorMessage" role="alert">
                                         <strong>Oops!</strong> Invalid youtube url id.
                                     </div>
-                                    <?php if(!empty($material)): ?>
-                                    <div class="attachment">
-                                        <p>
-                                            <span><i class="fa fa-paperclip"></i> <?= count($material); ?> attachments with this training </span>
-                                        </p>
-                                        <br>
-
-                                        <div class="border-box">
-
-                                            <span> <img src="<?= Yii::getAlias('@web/images/pdf.png')?>" alt="img"> Pdf  </span>
-
-                                        <ul>
-                                            <?php foreach($material as $mat): ?>
-                                            <li id="box-<?= $mat->id ?>">
-                                                <a href="#" class="atch-thumb">
-                                                <i class="fa fa-paperclip" aria-hidden="true"></i>
-                                                <?= $mat->original_name; ?>
-                                                <?= Html::button('<i class="fa fa-trash-o" aria-hidden="true"></i> ', ['class' => 'btn btn-danger btn-xs box-remove', 'id' => $mat->id])?>
-                                                </a>
- 
-                                            </li>
-                                            <?php endforeach; ?>
-                                        </ul>
+                                    <?php if (!empty($material)): ?>
+                                        <div class="attachment">
+                                            <p>
+                                                <span><i class="fa fa-paperclip"></i> <?= count($material); ?>
+                                                    attachments with this training </span>
+                                            </p>
+                                            <br>
 
 
+                                                    <?php foreach ($material as $mat): ?>
+
+
+                                                        <?php if ($mat->type == 0) {  //pdf ?>
+                                                            <div class="border-box">
+
+                                                                <span> <img src="<?= Yii::getAlias('@web/images/pdf.png') ?>" alt="img"> pdf  </span>
+
+                                                                <ul>
+                                                                    <li id="box-<?= $mat->id ?>">
+                                                                        <a href="#" class="atch-thumb">
+                                                                            <i class="fa fa-paperclip" aria-hidden="true"></i>
+                                                                            <?= $mat->original_name; ?>
+                                                                            <?= Html::button('<i class="fa fa-trash-o" aria-hidden="true"></i> ', ['class' => 'btn btn-danger btn-xs box-remove', 'id' => $mat->id]) ?>
+                                                                        </a>
+
+                                                                    </li>
+                                                                </ul>
+
+
+                                                            </div>
+                                                        <?php } elseif ($mat->type == 2) { //xls ?>
+                                                            <div class="border-box">
+
+                                                                <span> <img src="<?= Yii::getAlias('@web/images/pdf.png') ?>" alt="img"> xls/xlx  </span>
+
+                                                                <ul>
+                                                                    <li id="box-<?= $mat->id ?>">
+                                                                        <a href="#" class="atch-thumb">
+                                                                            <i class="fa fa-paperclip" aria-hidden="true"></i>
+                                                                            <?= $mat->original_name; ?>
+                                                                            <?= Html::button('<i class="fa fa-trash-o" aria-hidden="true"></i> ', ['class' => 'btn btn-danger btn-xs box-remove', 'id' => $mat->id]) ?>
+                                                                        </a>
+
+                                                                    </li>
+                                                                </ul>
+
+
+                                                            </div>
+                                                        <?php } elseif ($mat->type == 3) { //doc ?>
+                                                            <div class="border-box">
+
+                                                                <span> <img src="<?= Yii::getAlias('@web/images/pdf.png') ?>" alt="img"> doc/docx  </span>
+
+                                                                <ul>
+                                                                    <li id="box-<?= $mat->id ?>">
+                                                                        <a href="#" class="atch-thumb">
+                                                                            <i class="fa fa-paperclip" aria-hidden="true"></i>
+                                                                            <?= $mat->original_name; ?>
+                                                                            <?= Html::button('<i class="fa fa-trash-o" aria-hidden="true"></i> ', ['class' => 'btn btn-danger btn-xs box-remove', 'id' => $mat->id]) ?>
+                                                                        </a>
+
+                                                                    </li>
+                                                                </ul>
+
+
+                                                            </div>
+                                                        <?php } elseif ($mat->type == 4) { //ppt ?>
+                                                            <div class="border-box">
+
+                                                                <span> <img src="<?= Yii::getAlias('@web/images/pdf.png') ?>" alt="img"> ppt/pptx  </span>
+
+                                                                <ul>
+                                                                    <li id="box-<?= $mat->id ?>">
+                                                                        <a href="#" class="atch-thumb">
+                                                                            <i class="fa fa-paperclip" aria-hidden="true"></i>
+                                                                            <?= $mat->original_name; ?>
+                                                                            <?= Html::button('<i class="fa fa-trash-o" aria-hidden="true"></i> ', ['class' => 'btn btn-danger btn-xs box-remove', 'id' => $mat->id]) ?>
+                                                                        </a>
+
+                                                                    </li>
+                                                                </ul>
+
+
+                                                            </div>
+                                                        <?php } elseif ($mat->type == 1) { //mp4 ?>
+                                                            <div class="border-box">
+
+                                                                <span> <img src="<?= Yii::getAlias('@web/images/pdf.png') ?>" alt="img"> mp4  </span>
+
+                                                                <ul>
+                                                                    <li id="box-<?= $mat->id ?>">
+                                                                        <a href="#" class="atch-thumb">
+                                                                            <i class="fa fa-paperclip" aria-hidden="true"></i>
+                                                                            <?= $mat->original_name; ?>
+                                                                            <?= Html::button('<i class="fa fa-trash-o" aria-hidden="true"></i> ', ['class' => 'btn btn-danger btn-xs box-remove', 'id' => $mat->id]) ?>
+                                                                        </a>
+
+                                                                    </li>
+                                                                </ul>
+
+
+                                                            </div>
+                                                        <?php } ?>
+
+                                                    <?php endforeach; ?>
+
+
+                                            <br>
                                         </div>
-                              
-                                        <br>
-                                    </div>
                                         <div class="alert alert-success hide" id="deletedMessageBox" role="alert">
                                             <strong>Hurray!</strong> Image deleted successfully.
                                         </div>
-                                       
+
                                     <?php endif; ?>
-                                    
-                                    
-                                        <?= Html::submitButton('Save & Next', ['class' => 'btn btn-primary btnorange marginright btnsubmit','name' => 'tt', 'value' => 'start_training','data-type' => 'sent']) ?>
-                                        <?= Html::a('Close & Exit',['view','id' => Yii::$app->samparq->encryptUserData($tid) ],['class' => 'btn btn-danger']) ?>
+
+
+                                    <?= Html::submitButton('Save & Next', ['class' => 'btn btn-primary btnorange marginright btnsubmit', 'name' => 'tt', 'value' => 'start_training', 'data-type' => 'sent']) ?>
+                                    <?= Html::a('Close & Exit', ['view', 'id' => Yii::$app->samparq->encryptUserData($tid)], ['class' => 'btn btn-danger']) ?>
 
 
                                     <?php ActiveForm::end(); ?>
-                                            
+
                                 </div>
                             </div>
                         </div>
@@ -149,11 +233,11 @@ use dosamigos\fileupload\FileUploadUI;
                     </div>
                 </section>
             </div>
-         <br>
-</div>
- 
+            <br>
+        </div>
 
-</div>
+
+    </div>
 
 <?php
 $url = Url::toRoute(['image-delete']);
